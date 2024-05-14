c47da08d95b14c2663cd4339a1eb4ece567ecd23
Alessandro Belfiore <73615267+AlessandroBelfiore257@users.noreply.github.com>
2024-05-14 15:41:00 +0200

    Ridefiniti i nomi dei prototipi di funzione

--- src/Storage.h
+++ src/Storage.h
@@ -5,12 +5,10 @@
 #include "LCtime.h"
 
 extern sqlite3 *db;
-extern struct tm timeinfo;
 
-static int callback(void *data, int argc, char **argv, char **azColName);
+void initializeStorage();
 int db_open(const char *filename, sqlite3 **db);
 int db_exec(sqlite3 *db, const char *sql);
-void initializeStorage();
-struct tm getDateAndTime();
+void createRecordToInsertIntot1(char* name_sensor, int value, long long int ts, int synch, int pr);
 
 #endif
