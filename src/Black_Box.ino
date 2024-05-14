9554c29ea0196fb040d06fe539ec718cd83bcea2
Alessandro Belfiore <73615267+AlessandroBelfiore257@users.noreply.github.com>
2024-05-14 15:46:27 +0200

    Refactoring: migliorata la leggibilità del flusso di set up

--- src/Black_Box.ino
+++ src/Black_Box.ino
@@ -1,12 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <SPI.h>
-#include <Arduino.h>
-#include <FS.h>
-#include <LittleFS.h>
 #include <TaskScheduler.h>
-#include <WiFi.h>
-#include "time.h"
+#include <LittleFS.h>
 
 #include "Storage.h"
 #include "LCtime.h"
@@ -19,36 +14,41 @@ const char* password = WIFI_PASSWORD;
 const uint16_t port = PORT;
 const char* host = HOST;
 const int BUTTON_PIN = BUTTON;
+const char* data = "Callback function called";
 
 Scheduler scheduler;
 WiFiClient client;
-// Emulazione sensori veri e propri
-int mydata;
+sqlite3* db;
+struct tm timeinfo;
+// Rimpiazzo le variabili sottostanti con quelle dei sensori veri e propri della BB
+// Sensore numero casuale
+int mydata = 0;
+// Sensore button
 int buttonState = 0;
 
-int rc;
-bool passed;
-long long int lastStoreMillis;
+int rc = 0;
+bool passed = false;
+long long int lastStoredTS = 0;
 
 void rilevoButtonPressureCallback();
-void rilevoCasualNumberCallback();
+//void rilevoCasualNumberCallback();
 
 Task ButtonPressureTask(TASK_BUTTON_PRESSURE, TASK_FOREVER, &rilevoButtonPressureCallback);
-Task CasualNumberTask(TASK_CASUAL_NUMBER, TASK_FOREVER, &rilevoCasualNumberCallback);
-
-const char* data = "Callback function called";
+//Task CasualNumberTask(TASK_CASUAL_NUMBER, TASK_FOREVER, &rilevoCasualNumberCallback);
 
 void setup() {
   Serial.begin(115200);
+
   setTimeIT();
+
   // Richiesta di connessione che andrà in un task (polling)
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.println("...");
   }
-  Serial.print("WiFi connected with IP: ");
-  Serial.println(WiFi.localIP());
+  Serial.print("WiFi connected\n");
+
   // Gestione di montaggio e apertura del filesystem (verifica dei file presenti in esso)
   if (!LittleFS.begin(FORMAT_LITTLEFS_IF_FAILED, "/littlefs")) {
     Serial.println("Failed to mount file system");
@@ -99,15 +99,14 @@ void loop() {
 void rilevoButtonPressureCallback() {
   buttonState = digitalRead(BUTTON_PIN);
 
-  if (db_open("/littlefs/dati.db", &db))
-    return;
-
-  // query e verifico se è passato il tempo di campionamento:
-
   // Converte il timestamp attuale in millisecondi
   struct tm timeinfo = getDateAndTime();
-  time_t timestampNow_seconds = mktime(&timeinfo);
-  unsigned long timestampNow_millis = timestampNow_seconds;
+  time_t timestampNow_ret = mktime(&timeinfo);
+  unsigned long timestampNow = timestampNow_ret;
+
+  if (db_open("/littlefs/dati.db", &db))
+    return;
+  // Query e verifico se è passato il tempo di campionamento:
 
   const char* sql = "SELECT * FROM t1 WHERE nome_sensore = ? AND timestamp = ?";
   // Prepara la dichiarazione SQL
@@ -115,11 +114,12 @@ void rilevoButtonPressureCallback() {
   int rc = sqlite3_prepare_v2(db, sql, -1, &stmt, NULL);
   if (rc != SQLITE_OK) {
     Serial.println("Errore durante la preparazione della query SQL");
+    sqlite3_close(db);
     return;
   }
   // Associa i valori dei parametri (nome sensore e timestamp) alla query
   sqlite3_bind_text(stmt, 1, "Button pressure", -1, SQLITE_TRANSIENT);
-  sqlite3_bind_int64(stmt, 2, lastStoreMillis);
+  sqlite3_bind_int64(stmt, 2, lastStoredTS);
   // Esegui la query
   rc = sqlite3_step(stmt);
   if (rc == SQLITE_ROW) {
@@ -129,52 +129,45 @@ void rilevoButtonPressureCallback() {
     long long int timestamp = sqlite3_column_int64(stmt, 2);  // Modifica qui se il timestamp è un intero a 64 bit
     int sincronizzato = sqlite3_column_int(stmt, 3);
     int priorita = sqlite3_column_int(stmt, 4);
-
-    // Visualizza i valori dei campi
-    Serial.print("Nome sensore: ");
-    Serial.println(nome_sensore);
-    Serial.print("Timestamp: ");
-    Serial.println(timestamp);
+    Serial.print("Last registered record: ");
+    Serial.print(timestamp);
+    Serial.print(": ");
+    char prova[20];
+    SecondsToString(timestamp, prova);
+    Serial.println(prova);
   } else {
-    // Nessun record trovato
-    Serial.println("Nessun record trovato per il sensore specificato");
+    Serial.println("Nessun record trovato per il sensore specificato e il timestamp specificato");
   }
   // Rilascia la dichiarazione SQL
   sqlite3_finalize(stmt);
+  sqlite3_close(db);
 
-  // Verifica se è passato il tempo di campionamento --> politica di storage!!!
-  if ((timestampNow_millis - lastStoreMillis) >= 60) {
+  // Verifica se è passato il tempo di campionamento --> politica di storage!!! (Da rivedere)
+  if ((timestampNow - lastStoredTS) >= 60) {
     passed = true;
   } else {
     passed = false;
   }
-  Serial.print(timestampNow_millis);
+  Serial.print(timestampNow);
   Serial.print(" - ");
-  Serial.println(lastStoreMillis);
+  Serial.println(lastStoredTS);
   char b1[20];
   char b2[20];
-  SecondsToString(timestampNow_millis, b1);
-  SecondsToString(lastStoreMillis, b2);
+  SecondsToString(timestampNow, b1);
+  SecondsToString(lastStoredTS, b2);
   Serial.print(b1);
   Serial.print(" - ");
   Serial.println(b2);
 
   // Se è passato il tempo di campionamento, esegui le azioni desiderate
   if (passed) {
-    String fattaAmano = createTimeStampStringFormat(timeinfo.tm_mday, timeinfo.tm_mon + 1, timeinfo.tm_year + 1900, timeinfo.tm_hour, timeinfo.tm_min, timeinfo.tm_sec);
+    String timeStamp = createTimeStampStringFormat(timeinfo.tm_mday, timeinfo.tm_mon + 1, timeinfo.tm_year + 1900, timeinfo.tm_hour, timeinfo.tm_min, timeinfo.tm_sec);
     int syn = NO_SYNCHRONIZED;
-    createRecordToInsertIntot1("Button pressure", buttonState, fattaAmano.c_str(), syn, 2);
-    lastStoreMillis = timestampNow_millis;
+    long long int ts = stringToMilliseconds(timeStamp.c_str());
+    createRecordToInsertIntot1("Button pressure", buttonState, ts, syn, 2);
+    lastStoredTS = timestampNow;
 
-    Serial.println("DB/t1:");
-    rc = db_exec(db, "SELECT * FROM t1");
-    if (rc != SQLITE_OK) {
-      sqlite3_close(db);
-      return;
-    }
-
-    sqlite3_close(db);
-
+    // Fase di synch (In seguito)
     if (!client.connected()) {
     if (!client.connect(host, port)) {
       Serial.println("Connection to host failed");
@@ -184,7 +177,6 @@ void rilevoButtonPressureCallback() {
     }
   }
   SendToServer("Button pressure", buttonState, b1, 0, 2);
-
   } else {
     Serial.println("Mancato");
   }
@@ -196,9 +188,10 @@ void rilevoCasualNumberCallback() {
   if (db_open("/littlefs/dati.db", &db))
     return;
   struct tm timeinfo = getDateAndTime();
-  String fattaAmano = createTimeStampStringFormat(timeinfo.tm_mday, timeinfo.tm_mon + 1, timeinfo.tm_year + 1900, timeinfo.tm_hour, timeinfo.tm_min, timeinfo.tm_sec);
+  String timeStamp = createTimeStampStringFormat(timeinfo.tm_mday, timeinfo.tm_mon + 1, timeinfo.tm_year + 1900, timeinfo.tm_hour, timeinfo.tm_min, timeinfo.tm_sec);
   int syn = NO_SYNCHRONIZED;
-  createRecordToInsertIntot1("Casual number", mydata, fattaAmano.c_str(), syn, 1);
+  long long int ts = stringToMilliseconds(timeStamp.c_str());
+  createRecordToInsertIntot1("Casual number", mydata, ts, syn, 1);
   rc = db_exec(db, "SELECT * FROM t1");
   if (rc != SQLITE_OK) {
     sqlite3_close(db);
@@ -207,7 +200,8 @@ void rilevoCasualNumberCallback() {
 
   sqlite3_close(db);
 
-
+  // Fase di synch (In seguito)
+  /*
   if (!client.connected()) {
     if (!client.connect(host, port)) {
       Serial.println("Connection to host failed");
@@ -216,31 +210,9 @@ void rilevoCasualNumberCallback() {
       Serial.println("Connected to server successful!");
     }
   }
-
   SendToServer("Casual number", mydata, fattaAmano.c_str(), syn, 1);
-}
-
-/*
-  Creazione timestamp sotto forma di stringa 
-*/
-String createTimeStampStringFormat(int day, int month, int year, int hour, int minute, int second) {
-  return String(year) + "-" + String(month) + "-" + String(day) + " " + String(hour) + ":" + String(minute) + ":" + String(second);
-}
-
-/*
-  Creazione record e inserimento nella tabella t1 
-*/
-void createRecordToInsertIntot1(char* name_sensor, int value, const char* timestamp, int synch, int pr) {
-  long long int ts = stringToMilliseconds(timestamp);
-  char buffer[BUFFER_SIZE];
-  memset(buffer, 0, sizeof(buffer));
-  Serial.print("Not buffered: ");
-  Serial.println(buffer);
-  sprintf(buffer, "INSERT INTO t1 VALUES ('%s', %d, %lld, %d, %d);", name_sensor, value, ts, synch, pr);
-  Serial.print("Buffered: ");
-  Serial.println(buffer);
-  rc = db_exec(db, buffer);
-}
+  */
+} 
 
 /*
   Invio al server le informazioni utili
@@ -249,15 +221,24 @@ void SendToServer(char* name_sensor, int value, const char* timestamp, int synch
   client.print(String(name_sensor) + "," + String(value) + "," + timestamp + "," + String(synchronized) + "," + String(priority) + "\n");
 }
 
-// Funzione per convertire un timestamp sotto forma di stringa (char*) in millisecondi
+/*
+  Creazione timestamp sotto forma di stringa nel formato "YYYY-MM-DD HH:MM:SS"
+*/
+String createTimeStampStringFormat(int day, int month, int year, int hour, int minute, int second) {
+  return String(year) + "-" + String(month) + "-" + String(day) + " " + String(hour) + ":" + String(minute) + ":" + String(second);
+}
+
+/*
+  Funzione per convertire un timestamp sotto forma di stringa in secondi
+*/
 long long int stringToMilliseconds(const char* timestamp) {
   struct tm tm;
   strptime(timestamp, "%Y-%m-%d %H:%M:%S", &tm);
   time_t time_seconds = mktime(&tm);
-  return time_seconds;  // Moltiplicato per 1000 per ottenere i millisecondi
+  return time_seconds;
 }
 
-// Funzione per convertire un timestamp sotto forma di millisecondi in una stringa nel formato "YYYY-MM-DD HH:MM:SS"
+// Funzione per convertire un timestamp sotto forma di secondi in una stringa nel formato "YYYY-MM-DD HH:MM:SS"
 void SecondsToString(long long int total_seconds, char* date_string) {
   time_t raw_time = (time_t)total_seconds;
   struct tm* timeinfo = localtime(&raw_time);
