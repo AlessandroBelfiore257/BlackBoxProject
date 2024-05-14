a48d367ab9f4782f24a678efb40c5e4875ba964d
Alessandro Belfiore <73615267+AlessandroBelfiore257@users.noreply.github.com>
2024-05-14 15:42:09 +0200

    Ridefiniti i nomi dei prototipi di funzione

--- src/LCtime.h
+++ src/LCtime.h
@@ -2,12 +2,10 @@
 #define LCTIME_H
 
 #include <WiFi.h>
-#include "time.h"
-#include "sntp.h"
+#include <time.h>
+#include <sntp.h>
 
 void setTimeIT();
 struct tm getDateAndTime();
-void printLocalTime();
-void timeavailable(struct timeval *t);
 
 #endif
