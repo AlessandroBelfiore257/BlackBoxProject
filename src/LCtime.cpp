4cd13fc4643b87894f4bb5a021c6e3f09f96706b
Alessandro Belfiore <73615267+AlessandroBelfiore257@users.noreply.github.com>
2024-05-14 15:43:07 +0200

    Rese alcune funzioni private a livello di classe

--- src/LCtime.cpp
+++ src/LCtime.cpp
@@ -1,4 +1,5 @@
 #include "LCtime.h"
+
 #include "Secrets.h"
 #include "Config.h"
 
@@ -6,27 +7,6 @@ const char *ntpServer1 = NTP_SERVER1;
 const char *ntpServer2 = NTP_SERVER2;
 const long gmtOffset_sec = GMT_OFFSET;
 const int daylightOffset_sec = DAYLIGHT_OFFSET;
-
-void setTimeIT() {
-  sntp_set_time_sync_notification_cb(timeavailable);
-
-  /**
-   * NTP server address could be aquired via DHCP,
-   *
-   * NOTE: This call should be made BEFORE esp32 aquires IP address via DHCP,
-   * otherwise SNTP option 42 would be rejected by default.
-   * NOTE: configTime() function call if made AFTER DHCP-client run
-   * will OVERRIDE aquired NTP server address
-   */
-  sntp_servermode_dhcp(1);  // (optional)
-
-  /**
-   * This will set configured ntp servers and constant TimeZone/daylightOffset
-   * should be OK if your time zone does not need to adjust daylightOffset twice a year,
-   * in such a case time adjustment won't be handled automagicaly.
-   */
-  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer1, ntpServer2);
-}
 
 void printLocalTime() {
   struct tm timeinfo;
@@ -41,6 +21,25 @@ void printLocalTime() {
 void timeavailable(struct timeval *t) {
   Serial.println("Got time adjustment from NTP!");
   printLocalTime();
+}
+
+void setTimeIT() {
+  sntp_set_time_sync_notification_cb(timeavailable);
+  /**
+   * NTP server address could be aquired via DHCP,
+   *
+   * NOTE: This call should be made BEFORE esp32 aquires IP address via DHCP,
+   * otherwise SNTP option 42 would be rejected by default.
+   * NOTE: configTime() function call if made AFTER DHCP-client run
+   * will OVERRIDE aquired NTP server address
+   */
+  sntp_servermode_dhcp(1);  // (optional)
+  /**
+   * This will set configured ntp servers and constant TimeZone/daylightOffset
+   * should be OK if your time zone does not need to adjust daylightOffset twice a year,
+   * in such a case time adjustment won't be handled automagicaly.
+   */
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer1, ntpServer2);
 }
 
 struct tm getDateAndTime() {
