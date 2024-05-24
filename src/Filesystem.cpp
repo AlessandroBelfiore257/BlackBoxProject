75bb4702df4074cb09d26414f18bed9223a16269
Alessandro Belfiore <73615267+AlessandroBelfiore257@users.noreply.github.com>
2024-05-24 22:53:49 +0200

    Integrato il meccanismo di sincronizzazione

---
+++ src/Filesystem.cpp
@@ -0,0 +1,40 @@
+#include "Filesystem.h"
+
+#include <LittleFS.h>
+
+#include "Config.h"
+
+// Gestione di montaggio del filesystem
+bool mountingFS(char* s) {
+  if (!LittleFS.begin(FORMAT_LITTLEFS_IF_FAILED, s)) {
+    return 0;
+  } else {
+    return 1;
+  }
+}
+
+void openFS() {
+  File root = LittleFS.open("/");
+  if (!root) {
+    Serial.println("- failed to open directory");
+    return;
+  }
+  if (!root.isDirectory()) {
+    Serial.println(" - not a directory");
+    return;
+  }
+  File file = root.openNextFile();
+  while (file) {
+    if (file.isDirectory()) {
+      Serial.print("  DIR : ");
+      Serial.println(file.name());
+    } else {
+      Serial.print("  FILE: ");
+      Serial.print(file.name());
+      Serial.print("\tSIZE: ");
+      Serial.println(file.size());
+    }
+    file = root.openNextFile();
+  }
+  LittleFS.remove("/dati.db");
+}
