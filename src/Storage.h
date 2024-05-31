afb9459ebc494df71cafa818faf5dfe5c8a050f5
Alessandro Belfiore <73615267+AlessandroBelfiore257@users.noreply.github.com>
2024-05-31 18:49:23 +0200

    Introdotto prototipo funzione sul tempo di storage

--- src/Storage.h
+++ src/Storage.h
@@ -8,5 +8,6 @@ void initializeStorage(char* path);
 int db_open(const char *filename, sqlite3 **db);
 int db_exec(sqlite3 *db, const char *sql);
 void createRecordToInsertIntot1(char* name_sensor, const char* value, char* data_type, char* unit_of_measure, long long int ts, int synch, int pr);
+long long int getTempoStorage(sqlite3* db, const char* nome_sensore);
 
 #endif
