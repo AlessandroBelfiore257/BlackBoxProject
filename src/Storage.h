7e377f1b28fb5769066b8ae64423500448773ba0
Alessandro Belfiore <73615267+AlessandroBelfiore257@users.noreply.github.com>
2024-06-01 21:59:47 +0200

    Aggiunti prototipi per nuove funzioni

--- src/Storage.h
+++ src/Storage.h
@@ -9,5 +9,7 @@ int db_open(const char *filename, sqlite3 **db);
 int db_exec(sqlite3 *db, const char *sql);
 void createRecordToInsertIntot1(char* name_sensor, const char* value, char* data_type, char* unit_of_measure, long long int ts, int synch, int pr);
 long long int getTempoStorage(sqlite3* db, const char* nome_sensore);
+bool getSensorParameters(const char* sensorName, int& tempoStorage, int& fattoreIncrDecr, float& soglia, int& tMinStorage, int& tMaxStorage);
+void updateTempoStorage(const char* sensorName, int newTempoStorage);
 
 #endif
