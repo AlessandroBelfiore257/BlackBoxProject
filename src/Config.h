8273f84733d381e6cf94ad253c2e59ee429a7078
Alessandro Belfiore <73615267+AlessandroBelfiore257@users.noreply.github.com>
2024-06-03 19:28:37 +0200

    Aggiornato Config.h

--- src/Config.h
+++ src/Config.h
@@ -9,6 +9,12 @@
 #define DAYLIGHT_OFFSET 3600
 
 #define BUTTON_PIN 4
+#define ALCOOL_PIN 33
+#define AIR_QUALITY_PIN 32
+
+#define LED_ROSSO 13
+#define LED_GIALLO 14
+#define LED_VERDE 27
 
 #define FILE_SYSTEM "/littlefs"
 #define PATH_STORAGE_DATA_DB "/littlefs/dati.db"
@@ -16,9 +22,11 @@
 
 #define BUTTON_PRESSURE_DETECTION 10
 #define SYNCHRONIZATION_DATA 60 // adesso sincronizzo ogni 30 sec, nella versione ufficiale la sincronizzazione avverrà in presenza di connessione e con un tempo maggiore di 24h dall'ultima sincronizzazione
-#define CLEANING_ROUTINE 120 // adesso sono 60 secondi, nella versione ufficiale saranno 6h
+#define CLEANING_ROUTINE 120 // adesso sono 120 secondi, nella versione ufficiale saranno 6h
 #define CLEANING_MEMORY_FULL 300 
 #define GPS 30
+#define ACCELEROMETRIA 20
+#define AIR 25
 
 #define BUFFER_SIZE 1000
 
