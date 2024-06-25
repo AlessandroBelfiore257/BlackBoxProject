6c72bfc3d4a04a050c59bae2ae1be35286f8c072
Alessandro Belfiore <73615267+AlessandroBelfiore257@users.noreply.github.com>
2024-06-25 18:03:45 +0200

    Trattata la logica per la segnalazione di allarmi

---
+++ src/Prototipo2/Storage.h
@@ -0,0 +1,21 @@
+#ifndef STORAGE_H
+#define STORAGE_H
+
+#include <sqlite3.h>
+#include "LCtime.h"
+
+void initializeStorage(char* path);
+int db_open(const char *filename, sqlite3 **db);
+int db_exec(sqlite3 *db, const char *sql);
+void createRecordToInsertIntot1(char* name_sensor, const char* value, char* data_type, char* unit_of_measure, long long int ts, int synch, int pr);
+long long int getTempoStorage(sqlite3* db, const char* nome_sensore);
+bool getSensorParameters(const char* sensorName, int& tempoStorage, int& fattoreIncrDecr, float& soglia, int& tMinStorage, int& tMaxStorage);
+void updateTempoStorage(const char* sensorName, int newTempoStorage);
+// profilazione
+void createRecordToInsertIntot3(const char *date, const char *normalizedNumber, long long int lastStorage, int synchronised);
+long long int getLastTimestampFromt3(sqlite3 *db);
+// red allarmi
+void createRecordToInsertIntot4(const char *problema, const char *date, int synchronised);
+bool alreadyExists(sqlite3* db, const char* problema);
+
+#endif
