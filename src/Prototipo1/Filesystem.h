1538eac65e84226b7bab4b14d488a686ddbfb997
Alessandro Belfiore <73615267+AlessandroBelfiore257@users.noreply.github.com>
2024-07-04 13:51:51 +0200

    Eliminato src/Prototipo1 directory

--- src/Prototipo1/Filesystem.h
+++
@@ -1,7 +0,0 @@
-#ifndef FILESYSTEM_H
-#define FILESYSTEM_H
-
-bool mountingFS(char* s);
-void openFS();
-
-#endif
--- src/Prototipo1/Secrets.h
+++
@@ -1,9 +0,0 @@
-#ifndef SECRETS_H
-#define SECRETS_H
-
-#define WIFI_SSID "iPhone di Alessandro "
-#define WIFI_PASSWORD "12345676"
-
-#define PORT 8090
-#define HOST "172.20.10.4"
-#endif
